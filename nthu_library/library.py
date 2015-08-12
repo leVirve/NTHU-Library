b1205b6f23aad187046678ee3c436b552573cc33
leVirve <gae.m.project@gmail.com>
2015-08-12 16:11:50 +0800

    refactor user account partial functions

--- nthu_library/library.py
+++ nthu_library/library.py
@@ -1,9 +1,6 @@
-import re
-from urllib.parse import urljoin
-
-from nthu_library.tools import get_page, post_page, get_rss
+from nthu_library.tools import get_rss
 from nthu_library.crawler import get_circulation_links, crawl_top_circulations, crawl_lost_objects
-from nthu_library.user import NotLoginException
+from nthu_library.user import User as LibraryUser
 
 __author__ = 'salas'
 
@@ -17,59 +14,12 @@ class NTHULibrary(object):
     lost_found_url = 'http://adage.lib.nthu.edu.tw/find/search_it.php'
 
     def __init__(self, user):
-        self.user = user
-        self._session_url = ''
+        self.user = LibraryUser(self, user)
         self._circulation_links = get_circulation_links(self)
-        self.is_login = self._login()
 
     def __repr__(self):
         return '%s@library object' % self.user.account
 
-    def _login(self):
-        soup = get_page(urljoin(self.home, '?func=file&file_name=login1'))
-        login_url = soup.find('form').attrs.get('action')
-
-        resp = post_page(login_url, data=self.user.to_dict())
-        self._session_url = resp.url
-        return '您已登入' in resp.text
-
-    def _parse(self, soup):
-        if not self.is_login:
-            raise NotLoginException
-        tables = soup.find_all('table', attrs={'cellspacing': '2'})
-
-        # 圖書館流通狀態
-        status = {}
-        for row in tables[0].find_all('tr'):
-            cols = [e for e in row.children if str(e).strip()]
-            key = cols[0].text.strip()
-            val = cols[1].find('a').text.strip()
-            link = re.findall("'(.*?)'", cols[1].find('a').attrs.get('href'))[0]
-            status[key] = (val, link)
-
-        # 聯絡資料
-        person = {}
-        for row in tables[1].find_all('tr'):
-            cols = [e for e in row.children if str(e).strip()]
-            key = cols[0].text.strip() or '地址'
-            val = cols[1].text.strip()
-            person[key] = person[key] + val if key in person else val
-
-        # 管理資訊
-        manage = {}
-        for row in tables[2].find_all('tr'):
-            cols = [e for e in row.children if str(e).strip()]
-            key = cols[0].text.strip()
-            val = cols[1].text.strip()
-            if key == '讀者權限資料':
-                val = re.findall("borstatus='(.*)'", val)[0]
-            manage[key] = val
-
-        result = dict()
-        result['user'] = person
-        result['status'] = status
-        result['user']['manage'] = manage
-        return result
 
     def get_lost(self,
                  place='ALL', date_start='2015-02-10',
@@ -116,63 +66,13 @@ class NTHULibrary(object):
         return crawl_top_circulations(query)
 
     def get_info(self):
-        if not self.is_login:
-            raise NotLoginException
-        soup = get_page(urljoin(self._session_url, '?func=BOR-INFO'))
+        return self.user.get_info()
 
-        return self._parse(soup)
+    def get_current_borrow(self, res):
+        return self.user.get_current_borrow(res)
 
-    def get_current_bowrrow(self, res):
-        if not self.is_login:
-            raise NotLoginException
-        try:
-            soup = get_page(res['status']['目前借閱中清單'][1])
-            rows = soup.find('table', attrs={'cellspacing': '2', 'border': '0'}).find_all('tr')[1:]
-        except AttributeError:
-            return []
-        books = []
-        for row in rows:
-            cols = [e for e in row.children if str(e).strip()]
-            meta_dl = re.findall('(.*?)(\d+)', cols[5].text)[0]
-
-            book = {
-                'link': cols[0].find('a').attrs.get('href'),
-                'author': cols[2].text,
-                'title': cols[3].text.strip(' /'),
-                'publish_year': cols[4].text,
-                'deadline_status': meta_dl[0] if len(meta_dl) == 2 else None,
-                'deadline': meta_dl[1] if len(meta_dl) == 2 else meta_dl[0],
-                'publish_cost': cols[7].text,
-                'branch': cols[8].text,
-                'call_number': cols[9].text
-            }
-            books.append(book)
-        return books
-
-    def get_bowrrow_history(self, res):
-        if not self.is_login:
-            raise NotLoginException
-        try:
-            soup = get_page(res['status']['借閱歷史清單'][1])
-            rows = soup.find('table', attrs={'cellspacing': '2', 'border': '0'}).find_all('tr')[1:]
-        except AttributeError:
-            return []
-        books = []
-        for row in rows:
-            cols = [e for e in row.children if str(e).strip()]
-            meta_dl = re.findall('(.*?)(\d+)', cols[4].text)[0]
-            book = {
-                'link': cols[0].find('a').attrs.get('href'),
-                'author': cols[1].text,
-                'title': cols[2].text.strip(' /'),
-                'publish_year': cols[3].text,
-                'deadline_status': meta_dl[0] if len(meta_dl) == 2 else None,
-                'deadline': meta_dl[1] if len(meta_dl) == 2 else meta_dl[0],
-                'borrow_time': cols[6].text + ' ' + re.findall('>(.*?)<', str(cols[7]))[0],
-                'branch': cols[8].text
-            }
-            books.append(book)
-        return books
+    def get_borrow_history(self, res):
+        return self.user.get_borrow_history(res)
 
     def get_current_reserve(self):
         pass
@@ -181,29 +81,4 @@ class NTHULibrary(object):
         pass
 
     def get_reserve_history(self, res):
-        if not self.is_login:
-            raise NotLoginException
-        try:
-            soup = get_page(res['status']['預約歷史清單'][1])
-            rows = soup.find('table', attrs={'cellspacing': '2', 'border': '0'}).find_all('tr')[1:]
-        except AttributeError:
-            return []
-        books = []
-        for row in rows:
-            cols = [e for e in row.children if str(e).strip()]
-            book = {
-                'link': cols[0].find('a').attrs.get('href'),
-                'author': cols[1].text,
-                'title': cols[2].text.strip(' /'),
-                'publish_year': cols[3].text,
-                'history_date': cols[4].text,
-                'booking_date': cols[5].text,
-                'booking_valid': cols[6].text,
-                'book_return': cols[7].text,
-                'branch': cols[8].text,
-                'call_number': cols[9].text,
-                'branch_take': cols[10].text,
-                'book_status': cols[11].text
-            }
-            books.append(book)
-        return books
+        return self.user.get_reserve_history(res)
--- nthu_library/user.py
+++ nthu_library/user.py
@@ -1,3 +1,8 @@
+import re
+
+from urllib.parse import urljoin
+from nthu_library.tools import get_page, post_page, get_rss
+
 __author__ = 'salas'
 
 
@@ -5,16 +10,165 @@ class NotLoginException(Exception):
     pass
 
 
-class UserPayload:
+class Account:
 
-    def __init__(self, account, password):
-        self.account = account
+    def __init__(self, username, password):
+        self.username = username
         self.password = password
+        self.home_url = ''
+        self.session_url = ''
+        self.is_login = False
+
+    def login(self):
+        soup = get_page(urljoin(self.home_url, '?func=file&file_name=login1'))
+        login_url = soup.find('form').attrs.get('action')
+
+        resp = post_page(login_url, data=self.to_dict())
+        self.is_login = '您已登入' in resp.text
+        self.session_url = resp.url
+        return self.is_login
 
     def to_dict(self):
         return {
-            'bor_id': self.account,
+            'bor_id': self.username,
             'bor_verification': self.password,
             'func': 'login',
             'ssl_flag': 'Y',
         }
+
+
+class User:
+
+    def __init__(self, parent, account):
+        self.parent = parent
+        self.account = account
+        self.is_login = self.init_account()
+
+    def init_account(self):
+        self.account.home_url = self.parent.home
+        return self.account.login()
+
+    def get_info(self):
+        if not self.is_login:
+            raise NotLoginException
+        soup = get_page(urljoin(self.account.session_url, '?func=BOR-INFO'))
+
+        return self._parse(soup)
+
+    def get_reserve_history(self, res):
+        if not self.is_login:
+            raise NotLoginException
+        try:
+            soup = get_page(res['status']['預約歷史清單'][1])
+            rows = soup.find('table', attrs={'cellspacing': '2', 'border': '0'}).find_all('tr')[1:]
+        except AttributeError:
+            return []
+        books = []
+        for row in rows:
+            cols = [e for e in row.children if str(e).strip()]
+            book = {
+                'link': cols[0].find('a').attrs.get('href'),
+                'author': cols[1].text,
+                'title': cols[2].text.strip(' /'),
+                'publish_year': cols[3].text,
+                'history_date': cols[4].text,
+                'booking_date': cols[5].text,
+                'booking_valid': cols[6].text,
+                'book_return': cols[7].text,
+                'branch': cols[8].text,
+                'call_number': cols[9].text,
+                'branch_take': cols[10].text,
+                'book_status': cols[11].text
+            }
+            books.append(book)
+        return books
+
+    def get_current_borrow(self, res):
+        if not self.is_login:
+            raise NotLoginException
+        try:
+            soup = get_page(res['status']['目前借閱中清單'][1])
+            rows = soup.find('table', attrs={'cellspacing': '2', 'border': '0'}).find_all('tr')[1:]
+        except AttributeError:
+            return []
+        books = []
+        for row in rows:
+            cols = [e for e in row.children if str(e).strip()]
+            meta_dl = re.findall('(.*?)(\d+)', cols[5].text)[0]
+
+            book = {
+                'link': cols[0].find('a').attrs.get('href'),
+                'author': cols[2].text,
+                'title': cols[3].text.strip(' /'),
+                'publish_year': cols[4].text,
+                'deadline_status': meta_dl[0] if len(meta_dl) == 2 else None,
+                'deadline': meta_dl[1] if len(meta_dl) == 2 else meta_dl[0],
+                'publish_cost': cols[7].text,
+                'branch': cols[8].text,
+                'call_number': cols[9].text
+            }
+            books.append(book)
+        return books
+
+    def get_borrow_history(self, res):
+        if not self.is_login:
+            raise NotLoginException
+        try:
+            soup = get_page(res['status']['借閱歷史清單'][1])
+            rows = soup.find('table', attrs={'cellspacing': '2', 'border': '0'}).find_all('tr')[1:]
+        except AttributeError:
+            return []
+        books = []
+        for row in rows:
+            cols = [e for e in row.children if str(e).strip()]
+            meta_dl = re.findall('(.*?)(\d+)', cols[4].text)[0]
+            book = {
+                'link': cols[0].find('a').attrs.get('href'),
+                'author': cols[1].text,
+                'title': cols[2].text.strip(' /'),
+                'publish_year': cols[3].text,
+                'deadline_status': meta_dl[0] if len(meta_dl) == 2 else None,
+                'deadline': meta_dl[1] if len(meta_dl) == 2 else meta_dl[0],
+                'borrow_time': cols[6].text + ' ' + re.findall('>(.*?)<', str(cols[7]))[0],
+                'branch': cols[8].text
+            }
+            books.append(book)
+        return books
+
+    def _parse(self, soup):
+        if not self.is_login:
+            raise NotLoginException
+        tables = soup.find_all('table', attrs={'cellspacing': '2'})
+
+        # 圖書館流通狀態
+        status = {}
+        for row in tables[0].find_all('tr'):
+            cols = [e for e in row.children if str(e).strip()]
+            key = cols[0].text.strip()
+            val = cols[1].find('a').text.strip()
+            link = re.findall("'(.*?)'", cols[1].find('a').attrs.get('href'))[0]
+            status[key] = (val, link)
+
+        # 聯絡資料
+        person = {}
+        for row in tables[1].find_all('tr'):
+            cols = [e for e in row.children if str(e).strip()]
+            key = cols[0].text.strip() or '地址'
+            val = cols[1].text.strip()
+            person[key] = person[key] + val if key in person else val
+
+        # 管理資訊
+        manage = {}
+        for row in tables[2].find_all('tr'):
+            cols = [e for e in row.children if str(e).strip()]
+            key = cols[0].text.strip()
+            val = cols[1].text.strip()
+            if key == '讀者權限資料':
+                val = re.findall("borstatus='(.*)'", val)[0]
+            manage[key] = val
+
+        result = dict()
+        result['user'] = person
+        result['status'] = status
+        result['user']['manage'] = manage
+        return result
